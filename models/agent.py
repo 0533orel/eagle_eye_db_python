5b3bfeb6ed49f8d2dac587b9376e05e9cdd10542
orel <0533orel@gmail.com>
2025-06-26 15:49:11 +0300

    add method get_agent_by_id

--- models/agent.py
+++ models/agent.py
@@ -1,6 +1,6 @@
 class Agent:
     def __init__(self, id, code_name, real_name, location, status, missions_completed):
-        self._id = id
+        self.__id = id
         self.code_name = code_name
         self.real_name = real_name
         self.location = location
@@ -8,8 +8,8 @@ class Agent:
         self.missions_completed = missions_completed
 
     def __str__(self):
-        return (f"\n{self.__class__.__name__}\n"
-                f"Code name {self.code_name}\n"
+        return (f"\n{self.__class__.__name__} id: {self.__id}\n"
+                f"Code name: {self.code_name}\n"
                 f"Name: {self.real_name}\n"
                 f"Location: {self.location}\n"
                 f"Status: {self.status}\n"
